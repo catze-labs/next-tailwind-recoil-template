cf9147316a7fc00eb6da784d24d64ecaa9b3405c
aiiiden <hjjam100@gmail.com>
2022-10-21 02:58:33 +0900

    init Next, Tailwind, Recoil, React-Query

---
+++ requests/test.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+type Fact = {
+  fact: string;
+  length: number;
+};
+
+export const fetchCatFact = async () => {
+  try {
+    const { data } = await axios.get<Fact>("https://catfact.ninja/fact");
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data);
+    }
+  }
+};
+
+export const useCatFact = () => {
+  const { data, isLoading, isError } = useQuery(["catFact"], fetchCatFact);
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+};
