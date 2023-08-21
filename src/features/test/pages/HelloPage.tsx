fb2a02c14fadf1b7b2c1c34e17c100a306fa36bd
howdyfrom2019 <tjd1emd@naver.com>
2023-08-21 16:07:29 +0900

    Add react-hook-form & validation with signature

--- src/features/test/pages/HelloPage.tsx
+++ src/features/test/pages/HelloPage.tsx
@@ -3,6 +3,7 @@ import { useConnect, useSignMessage } from 'wagmi';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 
 import Counter from '@/features/test/components/Counter';
+import TestForm from '@/features/test/components/TestForm';
 import TestHeader from '@/features/test/components/TestHeader';
 import { useCatFact } from '@/requests/cat';
 
@@ -15,29 +16,14 @@ const HelloPage: NextPage = () => {
   const { connect } = useConnect({
     connector: new MetaMaskConnector(),
   });
-  const { signMessage } = useSignMessage({
-    message: 'Hello World',
-    onSuccess: (result) => {
-      console.log(result);
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
 
   return (
-    <div className="flex flex-col gap-4 min-h-screen justify-center items-center">
+    <div className="flex flex-col gap-4 min-h-screen justify-center items-center py-16">
       <button
         className="px-4 py-2 bg-blue-500 text-white rounded-xl font-bold"
         onClick={() => connect()}
       >
         Connect Wallet
-      </button>
-      <button
-        className="px-4 py-2 bg-blue-500 text-white rounded-xl font-bold"
-        onClick={() => signMessage()}
-      >
-        Sign Messages
       </button>
       <div className="w-64 flex flex-col gap-4">
         <TestHeader />
@@ -45,6 +31,7 @@ const HelloPage: NextPage = () => {
         {!isLoading && catfact && <p>{catfact.fact}</p>}
         <Counter />
       </div>
+      <TestForm />
     </div>
   );
 };
